ccdf2aae24b9b03efc209f37124bda8bf091a454
burts11 <maarcbs.54@gmail.com>
2020-10-27 17:59:41 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./Componentes/Formulario";
 import Cita from "./Componentes/Cita";
 
 function App() {
+  //Citas en LocalStorage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
   //Array de citas
-  const [citas, almacenarCitas] = useState([]);
+  const [citas, almacenarCitas] = useState(citasIniciales);
+
+  // useEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+    if(citasIniciales){
+      localStorage.setItem("citas",JSON.stringify(citas))
+    }else{
+      localStorage.setItem("citas",JSON.stringify([]))
+    }
+  }, [citas]);
 
   // Función que tome las citas actuales y agregue la nueva
   const crearCita = (cita) => {
     almacenarCitas([...citas, cita]);
     console.log(cita);
   };
+
+  // Elimanar cita por su ID
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    almacenarCitas(nuevasCitas);
+  };
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
 
   return (
     <Fragment>
@@ -20,16 +44,11 @@ function App() {
           <div className="one-half column">
             <Formulario crearCita={crearCita} />
           </div>
+          <h2>{titulo}</h2>
           <div className="one-half column">
-            {citas.map(cita => (
-                <Cita 
-                key={cita.id}
-                cita={cita}
-                
-                />
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
-            
-
           </div>
         </div>
       </div>
